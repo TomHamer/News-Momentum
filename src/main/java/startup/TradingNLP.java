094ca32505e812514e398c869d5acf0849213e35
William Shen <william.w.y.shen@gmail.com>
2018-01-12 21:30:11 +1100

    Add startup class

---
+++ src/main/java/startup/TradingNLP.java
@@ -0,0 +1,20 @@
+package startup;
+
+import lombok.extern.slf4j.Slf4j;
+import rss.RSSFeedPoller;
+
+
+/**
+ * Created by Tom on 18/12/2017.
+ */
+@Slf4j
+public class TradingNLP {
+
+    public static void main(String[] args) {
+        RSSFeedPoller poller = RSSFeedPoller.create();
+
+        log.info("Starting poller");
+        poller.run();
+    }
+
+}
